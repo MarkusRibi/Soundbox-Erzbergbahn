0c743d46e7e2f8f9c4eba332d948637575313a6f
MarkusRibi <74071437+MarkusRibi@users.noreply.github.com>
2023-07-25 13:41:22 +0200

    Update Code.ino

--- Code.ino
+++ Code.ino
@@ -1,9 +1,28 @@
 void setup() {
-  // put your setup code here, to run once:
+pinMode(2, INPUT); //Setzt genannten Pin auf INPUT
+pinMode(4, INPUT); //Setzt gennanten Pin auf INPUT
+pinMode(6, INPUT); //Setzt gennanten Pin auf INPUT
+pinMode(8, INPUT); //Setzt genannten Pin auf INPUT
+pinMode(10, INPUT); // Setzt genannten Pin auf INPUT
+pinMode(12, INPUT); //Setzt genannten Pin auf INPUT
 
+pinMode(3, OUTPUT); //Setzt genannten Pin auf OUTPUT
+pinMode(5, OUTPUT); //Setzt genannten Pin auf OUTPUT
+pinMode(7, OUTPUT); //Setzt genannten Pin auf OUTPUT
+pinMode(9, OUTPUT); //Setzt genannten Pin auf OUTPUT
+pinMode(11, OUTPUT); //Setzt genannten Pin auf OUTPUT
+pinMode(13, OUTPUT); //Setzt geannten Pin auf OUTPUT
 }
+int taste1 = 0;
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  taste1 = digitalRead(2);
+  
+  if(taste1 == HIGH){
+    digitalWrite(13, HIGH);
+  }
+  else{
+    digitalWrite(13, LOW);
+  }
 }
